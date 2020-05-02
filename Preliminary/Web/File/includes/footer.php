9eeebf97fb81780658e08e580ea5179007b665cb
AdaniKamal <adanikamal@gmail.com>
2020-05-02 18:54:40 +0800

    Add files via upload

---
+++ Preliminary/Web/File/includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; NEWS PORTAL 2018</p>
+      </div>
+      <!-- /.container -->
+    
